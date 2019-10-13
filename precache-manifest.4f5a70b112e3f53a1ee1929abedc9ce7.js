8e03c2693dce63be15e76623a1adcc0903ad533c
wizvee <jeonghye.dev@gmail.com>
2019-10-14 07:44:05 +0900

    Updates

---
+++ precache-manifest.4f5a70b112e3f53a1ee1929abedc9ce7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a508b1afab328a926b9211c4ea7bd92",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "303c8b74290515c02035",
+    "url": "/todo-app/static/css/main.30d50272.chunk.css"
+  },
+  {
+    "revision": "a408b15edba416ec446e",
+    "url": "/todo-app/static/js/2.237f77c1.chunk.js"
+  },
+  {
+    "revision": "303c8b74290515c02035",
+    "url": "/todo-app/static/js/main.558879b4.chunk.js"
+  },
+  {
+    "revision": "4a9d528a7ea424b93781",
+    "url": "/todo-app/static/js/runtime-main.d6ec3fc4.js"
+  }
+]);
