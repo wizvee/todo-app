5d07cc688cb4dc5c3181cdcf941e5bf04b17d750
wizvee <jeonghye.dev@gmail.com>
2019-10-14 07:39:47 +0900

    react-virtualized로 최적화

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,23 +7,25 @@ import {
 import cn from 'classnames';
 import './TodoListItem.scss';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked, color } = todo;
 
   return (
-    <div className="TodoListItem">
-      <div
-        className={cn('checkbox', { checked })}
-        style={{ color }}
-        onClick={() => onToggle(id)}
-      >
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text" style={{ color }}>
-          {text}
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          style={{ color }}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text" style={{ color }}>
+            {text}
+          </div>
         </div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
